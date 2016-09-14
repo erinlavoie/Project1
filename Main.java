91d6a1500f754aae99da5fdbecdfabeb38f26cc7
Tiffany Lam <tclam05@gmail.com>
2016-09-14 15:23:24 -0400

    minor change

--- Main.java
+++ Main.java
@@ -7,21 +7,19 @@
  */
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputDialog;
-
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import java.util.Optional;
-import javafx.stage.WindowEvent;
 
 public class Main extends Application
 {
